5cbedfd28ca52d95f60a748c1d5631d8ae0e1f57
Thomas Carlsen <thomascarlsen86@gmail.com>
2014-02-25 22:04:06 +0100

    adding a menu

---
+++ client/scripts/menu.js
@@ -0,0 +1,29 @@
+/*global $, document*/
+
+Meteor.startup(function () {
+    'use strict';
+    
+    $('#menu')
+        .affix({
+            offset: {
+                top: function () {
+                    return (this.top = $('.main-container').offset().top);
+                },
+                bottom: 0
+            }
+        })
+        .on('affixed.bs.affix', function () {
+            $(this).css({
+                right: $('.main-container').offset().left
+            });
+        })
+        .on('affixed-top.bs.affix', function () {
+            $(this).css({
+                right: 0
+            });
+        });
+    
+    $('body').scrollspy({
+        target: '#menu'
+    });
+});
